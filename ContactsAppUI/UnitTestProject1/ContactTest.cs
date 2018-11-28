e05ee84bcd2fc26260b35b81399c2586c42d454a
Анастасия <akira.can65@gmail.com>
2018-11-28 10:14:10 +0300

    Добавлен проект юнит-тестов

---
+++ ContactsAppUI/UnitTestProject1/ContactTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using ContactsApp;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+
+    public class ContactTest
+        {
+            private Contact _contact;
+
+            [SetUp]
+            public void InitContact()
+            {
+                _contact = new Contact();
+            }
+
+            [TestCase("Смирнов", "Выполняется, если фамилия удачно присваевается",
+                TestName = "Позитивный тест геттера Surname")]
+            [Category("SurnameTests")]
+            public void TestSurnameGet_CorrectValue(string expected, string message)
+            {
+                _contact.Surname = expected;
+                var actual = _contact.Surname;
+
+                Assert.AreEqual(expected, actual, "Геттер возвращает неправильную фамилию");
+            }
+
+            [TestCase("", "Исключение, если фамилия - пустая строка",
+                TestName = "Присвоение пустой строки в качестве фамилии",
+                Ignore = "Тест не актуален")]
+            [TestCase("Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов",
+                "Исключение, если фамилия длиннее 50 символов"
+                , TestName = "Присвоение неправильной фамилии больше 50 символов")]
+            public void TestSurnameSet_ArgumentException(string wrongSurname, string message)
+            {
+                Assert.ThrowsException<ArgumentException>(
+                    () => { _contact.Surname = wrongSurname; }, 
+                    message);
+            }
+
+
+        }
+    
+}
